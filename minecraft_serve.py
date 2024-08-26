5b3c15049a52466333e2c93894600dd7f1f75fbf
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2024-08-26 19:37:20 +0530

    ngrok minor fix

--- minecraft_serve.py
+++ minecraft_serve.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 
+#use this to if files are not accesible
+#sudo chmod -R 777 ./world/
+
 def isFirstRun():
     if os.path.isdir("./versions") & os.path.isdir("./libraries"):
         return False
@@ -11,8 +14,7 @@ if isFirstRun():
     subprocess.run(["bash", "minecraft.sh"])
 
 else:
-    subprocess.run(["java", "-Xms16G", "-Xmx16G", "-jar", "server.jar", "nogui"])
-
     with open("./.env") as e:
         print(f"./ngrok config add-authtoken {e.read().split('=')[1]}")
 
+    subprocess.run(["java", "-Xms16G", "-Xmx16G", "-jar", "server.jar", "nogui"])
